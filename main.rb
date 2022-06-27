f4f8db58877ca3876850b966d824118dc0b23ec8
reiya.wow <reiyasoundwow@gmail.com>
2022-06-27 13:11:32 +0000

    add 4files for Human

---
+++ main.rb
@@ -0,0 +1,15 @@
+require "./human"
+require "./animal"
+require "./thinkable"
+
+#インスタンスの作成
+tanaka = Human.new("田中太郎", 25, "電車")
+suzuki = Human.new("鈴木次郎", 30, "野球")
+sato   = Human.new("佐藤花子", 20, "映画")
+
+
+#メソッドを実行
+[tanaka, suzuki, sato].each do |one|
+  one.say
+  one.think
+end
